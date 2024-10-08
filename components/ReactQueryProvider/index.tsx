3ed78fca3505fb725700496aa3515105e0668365
Alireza_develop <ebadia256@gmail.com>
2024-10-08 15:00:33 +0330

    add react query provider in layout

--- components/ReactQueryProvider/index.tsx
+++ components/ReactQueryProvider/index.tsx
@@ -4,7 +4,8 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState<QueryClient>(() => new QueryClient({}));
+  //   const client: QueryClient = new QueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
